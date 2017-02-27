da55094d8a2b0b59301b0c7c0fde1a70567a36ef
Mentor <mentor@palokaj.co>
2017-02-27 16:38:33 +0100

    promise writing

---
+++ file-system/promiseusers.js
@@ -0,0 +1,30 @@
+const fs = require( 'fs' )
+
+const readJson = target => {
+	return new Promise( ( resolve, reject ) => {
+		fs.readFile( target, 'utf8', ( err, data ) => {
+			if ( err ) return reject( err )
+			// The resolve will send data to the .then() called after the execution of readjson
+			resolve( JSON.parse( data ) )
+		} )
+	} )
+}
+
+const writetxt = ( target, content ) => {
+	return new Promise( ( resolve, reject ) => {
+		fs.writeFile( target, content, err => {
+			if ( err ) return reject( err )
+			resolve( )
+		} )
+	} )
+}
+
+readJson( __dirname + '/users.json' ).then( parsedData => {
+	// parsedData is the result of JSON.Parse( data ) from the resolve of readJson
+	// parsedData is an array
+	for (var i = 0; i < parsedData.length; i++) {
+		let thefilepath = __dirname + '/' + parsedData[i].name + '.txt'
+		let thecontent = parsedData[i].age + ' ' + parsedData[i].hair
+		writetxt( thefilepath, thecontent )
+	}
+} ).catch( console.log.bind( console ) )
