afe50ddae9b4e9021efb50bcbbdec8f2410e833c
Mentor <mentor@palokaj.co>
2017-02-27 13:01:46 +0100

    Read files with counter

--- file-system/app.js
+++ file-system/app.js
@@ -1,20 +1,68 @@
 // Require the file system module
 const fs = require( 'fs' )
 
+// ///////////////////////////////
+// Create two files
+// ///////////////////////////////
+
 fs.writeFile( __dirname + '/temp.txt', 'Some words', err => {
 	// Throw an error if the file was not written
 	if ( err ) throw err
 	// Give some feedback
-	console.log( 'Everything went ok' )
+	console.log( 'Everything went ok for some words' )
 } )
 
-fs.readFile( __dirname + '/temp.txt', 'utf-8', ( err, data ) => {
-	// Throw error if there is an error
+fs.writeFile( __dirname + '/temp-2.txt', 'Some potatoes', err => {
+	// Throw an error if the file was not written
 	if ( err ) throw err
-	// Display contents of read file
-	console.log( 'The data is: ' + data )
-	console.log( `The data is: ${data}` )
+	// Give some feedback
+	console.log( 'Everything went ok for potatoes' )
 } )
+
+// ///////////////////////////////
+// Read all text files
+// ///////////////////////////////
+
+fs.readdir( __dirname, ( err, files ) => {
+	if ( err ) throw err
+	console.log( files )
+
+	// Filter out the files with .txt
+	let filteredfiles = files.filter( file => {
+		return file.indexOf( '.txt' ) >= 0 ? true : false
+	} )
+	console.log( filteredfiles )
+
+	// Loop over and read the text files
+	let filedata = []
+	let wehaveread = 0
+	for (var i = 0; i < filteredfiles.length; i++) {
+		// Read the file contents
+		fs.readFile( __dirname + '/' + filteredfiles[i], 'utf8', ( err, data ) => {
+			if ( err ) throw err
+			filedata.push( data )
+			wehaveread ++
+			if ( wehaveread == filteredfiles.length ) console.log( filedata )
+		} )
+	}
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // /////////////////////////////// 
 // Notes
