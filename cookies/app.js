01966212977cbb7de21ac567a81626093607d1eb
Mentor <mentor@palokaj.co>
2017-03-03 12:32:27 +0100

    console log cookies on the pugiverse page

--- cookies/app.js
+++ cookies/app.js
@@ -1,8 +1,10 @@
 const express = require( 'express' )
 const app = express( )
+const cookieparser = require( 'cookie-parser' )
 
 // This serves STATIC files, meaning they do not change and are not interpreted
 app.use( '/', express.static( __dirname + '/resources' ) )
+app.use( cookieparser( ) )
 
 
 // These templates NEVER reach the browser. They are converted to HTML which does reach the browser
@@ -10,6 +12,7 @@ app.set( 'view engine', 'pug' )
 app.set( 'views', __dirname + '/views' )
 
 app.get( '/pugified', ( req, res ) => {
+	console.log( req.cookies )
 	res.render( 'index' )
 } )
 
